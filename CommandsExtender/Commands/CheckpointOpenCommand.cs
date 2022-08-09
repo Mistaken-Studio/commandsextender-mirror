f80fde92e00d697df12b227e2b43f0f4667b468b
Xname <xnameserver@o2.pl>
2022-08-09 19:30:51 +0200

    checkpointopen command

---
+++ CommandsExtender/Commands/CheckpointOpenCommand.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CheckpointOpenCommand.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Interactables.Interobjects;
+using Interactables.Interobjects.DoorUtils;
+using Mistaken.API.Commands;
+
+namespace Mistaken.CommandsExtender.Commands
+{
+    [CommandSystem.CommandHandler(typeof(CommandSystem.ClientCommandHandler))]
+    internal class CheckpointOpenCommand : IBetterCommand
+    {
+        public override string Description => base.Description;
+
+        public override string[] Aliases => new string[] { "checkopen" };
+
+        public override string Command => "checkpointopen";
+
+        public override string[] Execute(ICommandSender sender, string[] args, out bool success)
+        {
+            var player = Player.Get(sender);
+            success = false;
+
+            if (player.Role.Type != RoleType.NtfCaptain)
+                return new string[] { "Musisz być kapitanem by użyć tej komendy" };
+
+            var door = Door.List.First(x => x.Type == DoorType.CheckpointEntrance).Base as CheckpointDoor;
+            if (door._subDoors.Any(x => x.TargetState))
+                return new string[] { "Drzwi od checkpoint'a są już otwarte" };
+
+            door.ToggleAllDoors(true);
+            door.ServerChangeLock(DoorLockReason.Warhead, true);
+
+            success = true;
+            return new string[] { "Zrobione" };
+        }
+    }
+}
